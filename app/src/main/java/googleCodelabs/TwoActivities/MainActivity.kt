dd117724d314de34af5ea409ce0153a5684e6be6
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2023-01-29 11:58:54 +0900

    8. Coding challenge
    MainActivity에서 버튼 세개 중 하나 클릭했을 때 ThirdActivity를 시작시키는 인텐트 발생시킴, 인텐트 extra에 몇번째 버튼인지 정보 넘겨줌

--- app/src/main/java/googleCodelabs/TwoActivities/MainActivity.kt
+++ app/src/main/java/googleCodelabs/TwoActivities/MainActivity.kt
@@ -15,6 +15,9 @@ private val TAG : String = MainActivity::class.java.simpleName.toString()
 val EXTRA_MESSAGE : String = "googleCodelabs.TwoActivities.extra.MESSAGE"
 
 //public
+val EXTRA_TEXT_BUTTON : String = "googleCodelabs.TwoActivities.extra.TEXT_BUTTON"
+
+//public
 val TEXT_REQUEST : Int = 1
 
 class MainActivity : AppCompatActivity() {
@@ -55,5 +58,19 @@ class MainActivity : AppCompatActivity() {
                 mReplyTextView.visibility = View.VISIBLE
             }
         }
+    }
+
+    fun textButtonClicked(view: View) {
+        val text_button_num : Int = when(view.id){
+            R.id.text_button1 -> 1
+            R.id.text_button2 -> 2
+            else -> 3
+        }
+        Log.d(TAG, "Text Button ${text_button_num} clicked!")
+
+        intent = Intent(this, ThirdActivity::class.java)
+        intent.putExtra(EXTRA_TEXT_BUTTON, text_button_num)
+
+        startActivity(intent)
     }
 }
